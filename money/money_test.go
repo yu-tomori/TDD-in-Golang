fa7044afd09c77af93ab8cc8d16020f4a59405bb
yu <peacetomori1118@gmail.com>
2021-05-27 16:57:43 +0900

    1.4 undefined: Dollor

---
+++ money/money_test.go
@@ -0,0 +1,13 @@
+package money
+
+import (
+	"testing"
+)
+
+func TestMoneyMultiplication(t *testing.T) {
+	five := Dollar{5}
+	five.times(2)
+	if 10 != five.amount() {
+		t.Errorf("five.amount() should be %d", 10)
+	}
+}
