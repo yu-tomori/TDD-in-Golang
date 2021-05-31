10eb47ec2fb669846a0c856ab6f109719e5707ef
yugaraxy <peacetomori1118@gmail.com>
2021-05-31 23:50:47 +0900

    Chapter 9,10,11: tests pass

--- money/money.go
+++ money/money.go
@@ -5,15 +5,15 @@ type Money struct {
 	amount int
 }
 
-func (m Money) Equals(c currency) bool {
-	return m.amount == c.quantity() && m.unit == c.name()
+func (m Money) Equals(c money) bool {
+	return m.amount == c.quantity() && m.unit == c.currency()
 }
 
 func (m Money) Times(multiplier int) Money {
 	return Money{unit: m.unit, amount: m.amount * multiplier}
 }
 
-func (m Money) name() string {
+func (m Money) currency() string {
 	return m.unit
 }
 
@@ -21,9 +21,9 @@ func (m Money) quantity() int {
 	return m.amount
 }
 
-type currency interface {
+type money interface {
 	quantity() int
-	name() string
+	currency() string
 }
 
 func Dollar(amount int) Money {
